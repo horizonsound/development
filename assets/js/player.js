34c4d89a13b6de9e9f0da93672b6fef18786306c
horizonsoundcreative <horizonsoundcreative@gmail.com>
2026-03-12 19:00:23 -0500

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,1 +1,51 @@
+<script>
+    let YT_PLAYER = null;
+  let YT_READY = false;
 
+  function onYouTubeIframeAPIReady() {
+    YT_READY = true;
+  }
+
+  function safeLoadSong(songId) {
+    if (!YT_READY) {
+      setTimeout(() => safeLoadSong(songId), 50);
+      return;
+    }
+    loadSong(songId);
+  }
+
+  function buildPlayer(youtubeId) {
+    if (!YT_READY || !ensurePlayerContainer()) {
+      setTimeout(() => buildPlayer(youtubeId), 50);
+      return;
+    }
+
+    if (!YT_PLAYER) {
+      YT_PLAYER = new YT.Player('player-container', {
+        videoId: youtubeId,
+        playerVars: {
+          autoplay: 1,
+          mute: 0,
+          controls: 1,
+          rel: 0
+        },
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    } else {
+      YT_PLAYER.loadVideoById(youtubeId);
+    }
+  }
+
+  function ensurePlayerContainer() {
+    return document.getElementById("player-container") !== null;
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+      advanceToNextSong(window.SELECTED_SONG_ID);
+    }
+  }
+
+</script>
