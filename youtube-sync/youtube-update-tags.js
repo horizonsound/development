c8d1fa9d132be6bd0d94010fe50078dc5600243c
horizonsoundcreative <horizonsoundcreative@gmail.com>
2026-04-08 09:17:39 -0500

    Update youtube-update-tags.js

--- youtube-sync/youtube-update-tags.js
+++ youtube-sync/youtube-update-tags.js
@@ -1,16 +1,25 @@
-// scripts/youtube-update-tags.js
 import { google } from "googleapis";
 
 export async function updateYoutubeTags(videoId, tags) {
-  const youtube = google.youtube("v3");
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.YOUTUBE_CLIENT_ID,
+    process.env.YOUTUBE_CLIENT_SECRET
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.YOUTUBE_REFRESH_TOKEN
+  });
+
+  const youtube = google.youtube({
+    version: "v3",
+    auth: oauth2Client
+  });
 
   await youtube.videos.update({
     part: "snippet",
     requestBody: {
       id: videoId,
-      snippet: {
-        tags
-      }
+      snippet: { tags }
     }
   });
 
