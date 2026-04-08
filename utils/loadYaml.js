0ded200b5e5caaa21f3c9802dd11a4710a4cf5a9
horizonsoundcreative <horizonsoundcreative@gmail.com>
2026-04-08 09:03:46 -0500

    Create loadYaml.js

---
+++ utils/loadYaml.js
@@ -0,0 +1,16 @@
+// scripts/utils/loadYaml.js
+import fs from "fs";
+import yaml from "js-yaml";
+
+const FEED_PATH = "./_data/youtube_feed.yml";
+
+export function loadSongsYaml() {
+  const raw = fs.readFileSync(FEED_PATH, "utf8");
+  const parsed = yaml.load(raw);
+  return parsed.songs || [];
+}
+
+export function writeSongsYaml(songs) {
+  const yamlStr = yaml.dump({ songs });
+  fs.writeFileSync(FEED_PATH, yamlStr, "utf8");
+}
