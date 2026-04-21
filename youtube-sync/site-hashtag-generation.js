d0248cef1dcb7e11cc221f2d2f2451636bbf6ce2
horizonsoundcreative <Kevin.Jandrow@HorizonSound.org>
2026-04-21 10:49:03 -0500

    Update site-hashtag-generation.js

--- youtube-sync/site-hashtag-generation.js
+++ youtube-sync/site-hashtag-generation.js
@@ -1,4 +1,23 @@
 // site-hashtag-generation.js
+
+import fs from "fs";
+import yaml from "js-yaml";
+
+const SONGS_PATH = "../_data/youtube_feed.yml";
+const SITE_PATH = "../_data/site.yml";
+
+export function loadSongsYaml() {
+  const file = fs.readFileSync(SONGS_PATH, "utf8");
+  const data = yaml.load(file);
+  return data.songs || [];
+}
+
+export function loadSiteYaml() {
+  const file = fs.readFileSync(SITE_PATH, "utf8");
+  const data = yaml.load(file);
+  return data;
+}
+
 import fs from "fs";
 import yaml from "js-yaml";
 import { loadSongsYaml, loadSiteYaml } from "./utils/loadYaml.js";
